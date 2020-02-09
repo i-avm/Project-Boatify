9036ba42ff2cbc32c879a3f3363711a7e2aea677
21Aby <abyvarghese333@gmail.com>
2020-02-09 20:17:28 +0530

    DB conn

--- BoatifyProject/BoatifyApp/models.py
+++ BoatifyProject/BoatifyApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Login(models.Model):
+    name = models.CharField(max_length=15)
+    email = models.EmailField()
+    password = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
--- BoatifyProject/BoatifyApp/views.py
+++ BoatifyProject/BoatifyApp/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
+from .models import Login
+
 
 # Create your views here.
 def welcome(request):
-    return render(request,'BoatifyApp/welcome.html')
+    return render(request, 'BoatifyApp/welcome.html')
+
+
 def homepage(request):
-    return render(request,'BoatifyApp/homepage.html')
+    return render(request, 'BoatifyApp/homepage.html')
+
+
 def login(request):
-    return render(request,'BoatifyApp/login.html')
+    return render(request, 'BoatifyApp/login.html')
+
+
 def register(request):
-    return render(request,'BoatifyApp/register.html')
+    if (request.method == 'POST'):
+        name_r = request.POST.get('namer')
+        email_r = request.POST.get('emailr')
+        password_r = request.POST.get('pwr')
+
+        c = Login(name=name_r, email=email_r, password=password_r)
+        c.save()
+        return render(request, 'BoatifyApp/register.html')
+    else:
+        return render(request, 'BoatifyApp/register.html')
