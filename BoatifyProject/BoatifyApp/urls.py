923dba6421fd9763096272845905564c72a77272
ASHIK ALAVUDHEEN <ashikalavudheen@gmail.com>
2020-02-13 10:52:50 -0330

    aslam commit

--- BoatifyProject/BoatifyApp/urls.py
+++ BoatifyProject/BoatifyApp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.welcome , name='welcome'),
     path('homepage', views.homepage, name='homepage'),
-    path('login', views.login, name='login'),
+    path('login/', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('registerpage', views.registerpage, name='registerpage'),
     path('contact', views.contact, name='contact'),
