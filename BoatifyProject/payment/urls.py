3375aa02cfce706e4c124cfa8c9cca41c4b057e1
Aby <abyvarghese333@gmail.com>
2020-03-15 13:05:24 +0530

    payment almost complete

--- BoatifyProject/payment/urls.py
+++ BoatifyProject/payment/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('qrcode',views.qrcode, name='qrcode'),
     path('seatres', views.seatres, name='seatres'),
     path('seatdetails', views.seatdetails, name='seatdetails'),
-    path('paytm', views.paytm, name='paytm'),
-    path('handlerequest/', views.handlerequest, name="HandleRequest"),
+    path('charge', views.charge, name='charge'),
+    path('success/<str:args>/', views.successMsg, name="success"),
     ]
--- BoatifyProject/payment/views.py
+++ BoatifyProject/payment/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 import pyqrcode
 from .models import Seatres
 from pyqrcode import QRCode
 
-#from PayTm import Checksum
+import stripe
+
+stripe.api_key = "sk_test_g16mqCpueK42qQBfKCkus9UW00JIRkOIIt"
 
 # Create your views here.
 
-MERCHANT_KEY = 'poESoXaHniaGcmKS'
 
 
 
@@ -89,49 +91,27 @@ def qrcode(request):
     url.svg("payment\static\payment\images\qrcode_ticket.svg", scale=8)
     return render(request, 'payment/qrcode.html')
 
-def paytm(request):
-    if request.method == "POST":
-        amount = request.POST.get('ffare')
+def charge(request):
+    if request.method == 'POST':
+        print('data: ',request.POST)
 
-        # order = Orders(name=name, email=email, phone=phone)
-        # order.save()
-        # Request paytm to transfer the amount to your account after payment by user
-        param_dict = {
+        amount = int(request.POST['ffare'])
 
-            'MID': 'qzcxrB88184026675230',
-            'ORDER_ID': '123',
-            #'User_Name': User.username,
-            'TXN_AMOUNT': str(amount),
-            #'CUST_ID': email,
-            #'INDUSTRY_TYPE_ID': 'Retail',
-            'WEBSITE': 'WEBSTAGING',
-            'CHANNEL_ID': 'WEB',
-            'CALLBACK_URL': 'http://127.0.0.1:8000/payment/handlerequest/',
+        customer = stripe.Customer.create(
+            email = request.user.email,
+            name = request.user.username,
+            source = request.POST['stripeToken']
+        )
 
-        }
-        #param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(param_dict, MERCHANT_KEY)
-        param_dict['CHECKSUMHASH'] = "asdfgh1243567hytgfvcasdfgh1243asdfgh1243asdfgh1243asdfgh1243asdfgh1243asdfgh1243asdfgh1243asdfgh1243asdfgh12"
-        return render(request, 'payment/paytm.html', {'param_dict': param_dict})
-    else:
-        return render(request, 'payment/checkout.html')
+        charge = stripe.Charge.create(
+            customer = customer,
+            amount = amount*100,
+            currency = 'inr',
+            description = 'Payment by '+request.user.username
+        )
 
+    return redirect(reverse('success', args=[amount]))
 
-@csrf_exempt
-def handlerequest(request):
-    # paytm will send you post request here
-    form = request.POST
-    response_dict = {}
-    for i in form.keys():
-        response_dict[i] = form[i]
-        if i == 'CHECKSUMHASH':
-            checksum = form[i]
-
-    #verify = Checksum.verify_checksum(response_dict, MERCHANT_KEY, checksum)
-
-
-    #if verify:
-        #if response_dict['RESPCODE'] == '01':
-    #        print('order successful')
-  #  else:
-    #    print('order was not successful because' + response_dict['RESPMSG'])
-    return render(request, 'payment/paymentstatus.html', {'response': response_dict})
+def successMsg(request, args):
+	amount = args
+	return render(request, 'payment/paymentstatus.html', {'amount':amount})
